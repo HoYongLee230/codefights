a559878a8864d0ac07c750366ef71a093439c5fb
HoYongLee230 <hleex230@gmail.com>
2018-06-08 21:57:53 -0400

    Update differentSquares.java

--- differentSquares.java
+++ differentSquares.java
@@ -1,14 +1,14 @@
 int differentSquares(int[][] matrix) {
     Set <List<Integer>> hs = new HashSet<>();
+    List<Integer> arrList;
     for(int i = 0; i < matrix.length - 1; i++) {
         for(int j = 0; j < matrix[0].length - 1; j++) {
-            List<Integer> l = new ArrayList<>();
+            arrList = new ArrayList<>();
             for(int k = i; k < i + 2; k++) {
-                for(int m = j; m < j + 2; m++) {
-                    l.add(matrix[k][m]);
-                }
+                for(int l = j; l < j + 2; l++)
+                    arrList.add(matrix[k][l]);
             }
-            hs.add(l);
+            hs.add(arrList);
         }
     }
     return hs.size();
